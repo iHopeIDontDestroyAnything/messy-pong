b659db6f5b8161693773f91753e85b795bfc37d7
iHopeIDontDestroyAnything <jakub.urban62@gmail.com>
2025-02-28 20:36:33 +0100

    Delete linuxgame directory

--- linuxgame/game.h
+++
@@ -1,6 +0,0 @@
-#ifndef GAME_H_
-#define GAME_H_
-
-#include "gameOBJs/gameObjects.h" 
-
-#endif
--- linuxgame/gameOBJs/gameObjects.h
+++
@@ -1,7 +0,0 @@
-#ifndef GAMEOBJ_H_
-#define GAMEOBJ_H_
-
-#include "ball.h"
-#include "player.h"
-
-#endif 
